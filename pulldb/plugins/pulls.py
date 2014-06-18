0d7a9a51a3e96da15a5212fa5e4cf82204bcedbf
Russell Heilling <russell@linx.net>
2014-06-18 23:22:20 +0100

    minor simplification of issue key fetching code

--- pulldb/plugins/pulls.py
+++ pulldb/plugins/pulls.py
@@ -23,10 +23,11 @@ class PullsController(controller.CementBaseController):
         resp, content = http_client.request(base_url + path)
         pull_list = json.loads(content)
         for pull in pull_list['results']:
-            print "%6s %4s %s %r" % (
+            print "%6s %4s %s %s %r" % (
                 pull['issue']['identifier'],
                 pull['issue']['pubdate'],
-                pull['issue']['name'],
+                pull['volume']['name'],
+                pull['issue']['issue_number'],
                 pull['pull']['read'],
             )
 
@@ -51,5 +52,87 @@ class PullsController(controller.CementBaseController):
         else:
             self.app.log.error(resp, content)
 
+    @controller.expose()
+    def unread(self):
+        auth_handler = handler.get('auth', 'oauth2')()
+        auth_handler._setup(self.app)
+        http_client = auth_handler.client()
+        base_url = self.app.config.get('base', 'base_url')
+        path = '/api/pulls/unread'
+        resp, content = http_client.request(base_url + path)
+        pull_list = json.loads(content)
+        for pull in pull_list['results']:
+            print "%6s %10s %s %s" % (
+                pull['issue']['identifier'],
+                pull['issue']['pubdate'],
+                pull['volume']['name'],
+                pull['issue']['issue_number'],
+            )
+
+class UpdatePulls(controller.CementBaseController):
+    class Meta:
+        label = 'pull_update'
+        stacked_on = 'pull'
+        stacked_type = 'nested'
+        aliases = ['update']
+        aliases_only = True
+        arguments = [
+            (['issues'], {
+                'help': 'Comicvine issue ids',
+                'action': 'store',
+                'nargs': '+',
+            }),
+        ]
+
+    def post_list(self, path, list_key):
+        auth_handler = handler.get('auth', 'oauth2')()
+        auth_handler._setup(self.app)
+        http_client = auth_handler.client()
+        base_url = self.app.config.get('base', 'base_url')
+        data = json.dumps({
+            list_key: self.app.pargs.issues,
+        })
+        resp, content = http_client.request(
+            base_url + path,
+            method='POST',
+            headers={'Content-Type': 'application/json'},
+            body=data,
+        )
+        if resp.status != 200:
+            self.app.log.error('%r %r' % (resp, content))
+        else:
+            results = json.loads(content)
+            failed = results['results'].get('failed', [])
+            print '%d issues failed:\n%r' % (len(failed), failed)
+            added = results['results'].get('added', [])
+            print '%d issues added:\n%r' % (len(added), added)
+            updated = results['results'].get('updated', [])
+            print '%d issues updated:\n%r' % (len(updated), updated)
+            skipped = results['results'].get('skipped', [])
+            print '%d issues skipped:\n%r' % (len(skipped), skipped)
+
+    @controller.expose(hide=True)
+    def default(self):
+        self.app.args.print_help()
+
+    @controller.expose()
+    def add(self):
+        path = '/api/pulls/add'
+        list_key = 'issues'
+        self.post_list(path, list_key)
+
+    @controller.expose()
+    def read(self):
+        path = '/api/pulls/update'
+        list_key = 'read'
+        self.post_list(path, list_key)
+
+    @controller.expose()
+    def unread(self):
+        path = '/api/pulls/update'
+        list_key = 'unread'
+        self.post_list(path, list_key)
+
 def load():
     handler.register(PullsController)
+    handler.register(UpdatePulls)
